98dc7a81da22fe532184f621ff7a3aec67c48fcc
mathieu.valette1 <mathieu.valette1@gmail.com>
2022-02-01 09:27:14 +0100

    Initial commit

---
+++ src/main/java/com/tp/capitalix/CapitalixApplication.java
@@ -0,0 +1,13 @@
+package com.tp.capitalix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalixApplication.class, args);
+	}
+
+}
